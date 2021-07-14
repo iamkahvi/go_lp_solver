16290732e6170e5eed1cd182a785a83c14542b67
Kahvi Patel <iamkahvi@gmail.com>
2021-07-14 12:44:09 -0700

    Adding tests, reading from stdin

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strconv"
 	s "strings"
@@ -17,35 +17,53 @@ func check(e error) {
 }
 
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Printf("Usage 'go run main.go [file]'\n")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	lines := make([]string, 0)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if len(lines) <= 1 {
+		fmt.Printf("Usage 'go run main.go < [file]'\n")
 		os.Exit(0)
 	}
-	filename := os.Args[1]
 
-	dat, err := ioutil.ReadFile(filename)
-	check(err)
-
-	lines := s.Split(string(dat), "\n")
 	r, c := getDims(lines)
-
 	m := makeMatrix(lines, r, c)
-	negativeMatrix := mat.NewDense(r, c, make([]float64, r*c))
-	negativeMatrix.Apply(func(i, j int, v float64) float64 {
-		return -1
-	}, negativeMatrix)
+	nMatrix := makeNegMatrix(r, c)
 
-	fa := mat.Formatted(m, mat.Prefix("    "), mat.Squeeze())
-	fmt.Printf("a = %v\n\n", fa)
+	fm := mat.Formatted(m, mat.Prefix("    "), mat.Squeeze())
+	fmt.Printf("a = %v\n\n", fm)
 
-	m.MulElem(m, negativeMatrix)
-	fat := mat.Formatted(m.T(), mat.Prefix("    "), mat.Squeeze())
+	// inverseMatrix := mat.NewDense(r, c, make([]float64, r*c))
+	// inverseMatrix.Inverse(m)
+	// fim := mat.Formatted(inverseMatrix, mat.Prefix("    "), mat.Squeeze())
+	// fmt.Printf("a = %v\n\n", fim)
+
+	negMatrix := mat.NewDense(r, c, make([]float64, r*c))
+	negMatrix.MulElem(m, nMatrix)
+	fat := mat.Formatted(negMatrix.T(), mat.Prefix("    "), mat.Squeeze())
 	fmt.Printf("a = %v\n\n", fat)
+
+	vct := mat.Formatted(m.ColView(0))
+	fmt.Printf("%v\n\n", vct)
+
+	rw := mat.Formatted(m.RowView(0))
+	fmt.Printf("%v\n\n", rw)
 
 	rows, cols := m.Dims()
 	fmt.Printf("(%v, %v)\n", rows, cols)
 
 	fmt.Printf("matrix: %T\n", m)
+}
+
+func makeNegMatrix(rows int, cols int) *mat.Dense {
+	negativeMatrix := mat.NewDense(rows, cols, make([]float64, rows*cols))
+	negativeMatrix.Apply(func(i, j int, v float64) float64 {
+		return -1
+	}, negativeMatrix)
+	return negativeMatrix
 }
 
 func makeMatrix(lines []string, rows int, cols int) *mat.Dense {
